07f30044ba72520924d5c31c55c4c9485275fa01
cuongtd <trancuong200495@gmail.com>
2021-06-08 16:06:02 +0700

    add quotes data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import firebase from "firebase";
+import Papa from "papaparse";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+const csv_url =
+  "https://raw.githubusercontent.com/akhiltak/inspirational-quotes/master/Quotes.csv";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    firestore
+      .collection("quotes")
+      .get()
+      .then((snapshot) => {
+        console.log("snapshot", snapshot);
+        if (snapshot.size === 0) {
+          return fetch(csv_url).then(async function (response) {
+            let reader = response.body.getReader();
+            let decoder = new TextDecoder("utf-8");
+            const result = await reader.read(); // raw array
+            const csv = decoder.decode(result.value); // the csv text
+            const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
+            const rows = results.data;
+
+            await Promise.all(
+              rows.map(async (row) => {
+                return firestore.collection("quotes").doc(row.QUOTE).set(row);
+              })
+            );
+            alert(`seed successfully ${rows.length} quotes`);
+          });
+        } else {
+          console.log("number of quotes: ", snapshot.size);
+        }
+      });
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
